4ae5e8882f9fa188a81e831de44cc8254ceba383
Anton Zyryanov <antonzyryanov99@gmail.com>
2026-02-08 07:01:16 +0300

    refactor: удален ненужный комментарий

--- lib/design_configs/app_bar_config.dart
+++ lib/design_configs/app_bar_config.dart
@@ -2,5 +2,5 @@ class AppBarConfig {
   static const double toolbarHeight = 70;
   static const double appBarElevation = 4;
   static const double appNameFontSize = 24;
-  static const double appNameFontWeight = 700; // FontWeight.bold
+  static const double appNameFontWeight = 700;
 }
